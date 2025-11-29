2d1e959ff7e34a909006e7a6b610fa37fac4775b
Yasin <abdyassin1391@gmail.com>
2025-11-29 20:48:11 +0330

    responsive full

--- vite-project/src/components/forgetPass/ForgetPassBox_1.jsx
+++ vite-project/src/components/forgetPass/ForgetPassBox_1.jsx
@@ -4,12 +4,12 @@ import RegisterButton from "../register/Register_button";
 const ForgetPassBox_1 = () => {
   return (
     
-    <div className="flex flex-col justify-center items-center bg-[#F5F5F5] shadow-box rounded-t-[80px] mt-0 md:mt-[100px] h-1/1 w-1/1 md:rounded-[20px] md:w-[561px] md:h-[491px] px-[30px] py-[64px]">
-      <p className="text-lg select-none">{"خوش امدید :)"}</p>
+    <div className="flex flex-col md:justify-center items-center bg-[#F5F5F5] shadow-box rounded-t-[80px] md:mt-[80px] w-1/1 md:rounded-[20px] md:w-[561px] md:h-[491px] px-[30px] py-[64px]">
+      <p className="text-lg select-none mt-10 md:mt-0">{"خوش امدید :)"}</p>
       <h3 className="text-xl font-bold mt-[4px] select-none mx-auto">
         {"ایجاد حساب کاربری"}
       </h3>
-      <div className="relative w-[430px]">
+      <div className="relative w-[345px] md:w-[430px]">
         <input
           className="text-sm font-semibold rtl w-1/1 bg-[#4B4B4B14] select-none h-[48px] border-0 pl-[16px] pr-[40px] py-[4px] rounded-[8px]  mt-[32px]"
           placeholder="ایمیل"
@@ -21,14 +21,8 @@ const ForgetPassBox_1 = () => {
       </div>
       <RegisterButton
         innerHtml="ریافت کد تایید"
-        className="bg-[#3C8B85] text-white w-[430px]"
+        className="bg-[#3C8B85] text-white w-[345px] md:w-[430px] "
       />
-      <div className="w-1/1">
-        <p className="text-blue-500 underline inline mt-1">دذئذس سیتانیستا</p>
-
-        <input type="checkbox" className="float-right inline m-2" />
-        <p className="inline float-right mt-1 cursor-pointer">تذینزتذ یتینیذست</p>
-      </div>
     </div>
     
   );
--- vite-project/src/config/router/index.js
+++ vite-project/src/config/router/index.js
@@ -5,9 +5,9 @@ import MainBox_step_1 from "../../components/register/MainBox_step_1";
 import MainBox_step_2 from "../../components/register/MainBox_step_2";
 import MainBox_step_3 from "../../components/register/MainBox_step_3";
 
-
 import ForgetPass from "../../screens/ForgetPassPage";
 import ForgetPassBox_1 from "../../components/forgetPass/ForgetPassBox_1";
+import ForgetPassBox_2 from "../../components/forgetPass/ForgetPassBox_2";
 const Router = createBrowserRouter([
   {
     path: "/register",
@@ -25,7 +25,10 @@ const Router = createBrowserRouter([
   {
     path: "/forgetPass",
     Component: ForgetPass,
-    children: [{ path: "step1", Component:ForgetPassBox_1 }],
+    children: [
+      { path: "step1", Component: ForgetPassBox_1 },
+      { path: "step2", Component: ForgetPassBox_2 },
+    ],
   },
 ]);
 export { Router };
