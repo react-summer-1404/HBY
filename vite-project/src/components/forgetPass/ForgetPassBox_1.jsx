e118c7f6a918482774dca86206715e47a4f3363d
Yasin <abdyassin1391@gmail.com>
2025-12-12 20:37:08 +0330

    forgetpass

--- vite-project/src/components/forgetPass/ForgetPassBox_1.jsx
+++ vite-project/src/components/forgetPass/ForgetPassBox_1.jsx
@@ -1,30 +1,65 @@
 import React from "react";
 import RegisterButton from "../register/Register_button";
-
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import apiClient from "../../core/services/interceptor";
+import * as yup from "yup";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { setData } from "../../core/localStorage/localStorage";
 const ForgetPassBox_1 = () => {
+  const validation = yup.object({
+    email: yup.string().required("بنویس").email("ایمیلٍ لامسب"),
+  });
+  const [Loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+  const postApi = async (data) => {
+    try {
+      setLoading(false);
+
+      const res = await apiClient.post("/Sign/ForgetPassword", data);
+      return res.data;
+    } catch (error) {
+      console.log(error, "dddddddddddd");
+    }
+  };
+  const onSubmit = (values) => {
+    setData("email",values.email);
+    values.baseUrl = "https://localhost:5173/resetpassword";
+    postApi(values);
+    navigate("/forgetpass/step2");
+  };
   return (
-    
-    <div className="flex flex-col md:justify-center items-center bg-[#F5F5F5] shadow-box rounded-t-[80px] md:mt-[80px] w-1/1 md:rounded-[20px] md:w-[561px] md:h-[491px] px-[30px] py-[64px]">
-      <p className="text-lg select-none mt-10 md:mt-0">{"خوش امدید :)"}</p>
-      <h3 className="text-xl font-bold mt-[4px] select-none mx-auto">
-        {"ایجاد حساب کاربری"}
-      </h3>
-      <div className="relative w-[345px] md:w-[430px]">
-        <input
-          className="text-sm font-semibold rtl w-1/1 bg-[#4B4B4B14] select-none h-[48px] border-0 pl-[16px] pr-[40px] py-[4px] rounded-[8px]  mt-[32px]"
-          placeholder="ایمیل"
+    <Formik
+      initialValues={{ email: "" }}
+      onSubmit={(values) => {
+        onSubmit(values);
+      }}
+      validationSchema={validation}
+    >
+      <Form className="flex flex-col md:justify-center items-center bg-[#F5F5F5] shadow-box rounded-t-[80px] md:mt-[80px] w-1/1 md:rounded-[20px] md:w-[561px] md:h-[491px] px-[30px] py-[64px]">
+        <p className="text-lg select-none mt-10 md:mt-0">{"خوش امدید :)"}</p>
+        <h3 className="text-xl font-bold mt-[4px] select-none mx-auto">
+          {"ایجاد حساب کاربری"}
+        </h3>
+        <div className="relative w-[345px] md:w-[430px]">
+          <Field
+            name="email"
+            className="text-sm font-semibold rtl w-1/1 bg-[#4B4B4B14] select-none h-[48px] border-0 pl-[16px] pr-[40px] py-[4px] rounded-[8px]  mt-[32px]"
+            placeholder="ایمیل"
+          />
+          <ErrorMessage name="email" />
+          <img
+            src="/register/email.png"
+            className="h-[20px] w-[20px] absolute bottom-[10.5px] right-[8px] my-1 "
+          />
+        </div>
+        <RegisterButton
+          innerHtml="ریافت کد تایید"
+          className="bg-[#3C8B85] text-white w-[345px] md:w-[430px] "
         />
-        <img
-          src="/register/email.png"
-          className="h-[20px] w-[20px] absolute bottom-[10.5px] right-[8px] my-1 "
-        />
-      </div>
-      <RegisterButton
-        innerHtml="ریافت کد تایید"
-        className="bg-[#3C8B85] text-white w-[345px] md:w-[430px] "
-      />
-    </div>
-    
+      </Form>
+    </Formik>
   );
 };
 
--- vite-project/src/components/forgetPass/ForgetPassBox_2.jsx
+++ vite-project/src/components/forgetPass/ForgetPassBox_2.jsx
@@ -1,43 +1,82 @@
 import React from "react";
 import RegisterButton from "../register/Register_button";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import apiClient from "../../core/services/interceptor";
+import * as yup from "yup";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { getData } from "../../core/localStorage/localStorage";
 
 const ForgetPassBox_1 = () => {
+  const validation = yup.object({
+    newPassword: yup.string().min(6, "8تا").required("بنویس"),
+    resetValue:yup.string().max(6,"6تا").required("بنویس")
+  });
+  const [Loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+  const postApi = async (data) => {
+    try {
+      setLoading(false);
+      const res = await apiClient.post("/Sign/Reset", data);
+    } catch (error) {
+      console.log(error, "dddddddddddd");
+    }
+  };
+  const onSubmit = (values) => {
+    values.email=getData("email");
+    console.log(values);
+    postApi(values);
+    navigate("/forgetpass/step2");
+  };
   return (
-    <div className="flex flex-col justify-center items-center bg-[#F5F5F5] shadow-box rounded-t-[80px] mt-0 md:mt-[70px] h-1/1 w-1/1 md:rounded-[20px] md:w-[561px] md:h-[491px] px-[30px] py-[64px]">
-      <p className="text-lg select-none">{"خوش امدید :)"}</p>
-      <h3 className="text-xl font-bold mt-[4px] select-none mx-auto">
-        {"ایجاد حساب کاربری"}
-      </h3>
-      <div className="relative md:w-[430px] w-[345px]">
-        <div className="relative w-[345px] md:w-[430px]">
-          <input
-            className="text-sm font-semibold rtl w-1/1 bg-[#4B4B4B14] select-none h-[48px] border-0 pl-[16px] pr-[40px] py-[4px] rounded-[8px]  mt-[32px]"
-            placeholder="رمز عبور"
-            type="password"
-          />
-          <img
-            src="/register/password.png"
-            className="h-[20px] w-[20px] absolute bottom-[10.5px] right-[8px] my-1 "
-          />
+    <Formik
+      initialValues={{ newPassword: "", resetValue: "" }}
+      onSubmit={(values) => {
+        onSubmit(values);
+      }}
+      validationSchema={validation}
+    >
+      <Form className="flex flex-col justify-center items-center bg-[#F5F5F5] shadow-box rounded-t-[80px] mt-0 md:mt-[70px] h-1/1 w-1/1 md:rounded-[20px] md:w-[561px] md:h-[491px] px-[30px] py-[64px]">
+        <p className="text-lg select-none">{"خوش امدید :)"}</p>
+        <h3 className="text-xl font-bold mt-[4px] select-none mx-auto">
+          {"ایجاد حساب کاربری"}
+        </h3>
+        <div className="relative md:w-[430px] w-[345px]">
+          <div className="relative w-[345px] md:w-[430px]">
+            <Field
+              name="newPassword"
+              className="text-sm font-semibold rtl w-1/1 bg-[#4B4B4B14] select-none h-[48px] border-0 pl-[16px] pr-[40px] py-[4px] rounded-[8px]  mt-[32px]"
+              placeholder="رمز عبور"
+              type="password"
+            />
+
+            <img
+              src="/register/password.png"
+              className="h-[20px] w-[20px] absolute bottom-[10.5px] right-[8px] my-1 "
+            />
+            <ErrorMessage name="newPassword" />
+          </div>
+          <div className="relative w-[345px] md:w-[430px]">
+            <Field
+              name="resetValue"
+              className="text-sm font-semibold rtl w-1/1 bg-[#4B4B4B14] select-none h-[48px] border-0 pl-[16px] pr-[40px] py-[4px] rounded-[8px]  mt-[32px]"
+              placeholder="کد"
+            />
+            <img
+              src="/register/password.png"
+              className="h-[20px] w-[20px] absolute bottom-[10.5px] right-[8px] my-1 "
+            />{" "}
+            <ErrorMessage name="resetValue" />
+          </div>
         </div>
-        <div className="relative w-[345px] md:w-[430px]">
-          <input
-            className="text-sm font-semibold rtl w-1/1 bg-[#4B4B4B14] select-none h-[48px] border-0 pl-[16px] pr-[40px] py-[4px] rounded-[8px]  mt-[32px]"
-            placeholder="تایید رمز عبور"
-            type="password"
-          />
-          <img
-            src="/register/password.png"
-            className="h-[20px] w-[20px] absolute bottom-[10.5px] right-[8px] my-1 "
-          />
-        </div>
-      </div>
-      <RegisterButton
-        innerHtml="ریافت کد تایید"
-        className="bg-[#3C8B85] text-white md:w-[430px] w-[345px]"
-      />
-      
-    </div>
+
+        <RegisterButton
+          innerHtml="ریافت کد تایید"
+          className="bg-[#3C8B85] text-white md:w-[430px] w-[345px]"
+        />
+      </Form>
+    </Formik>
   );
 };
 
