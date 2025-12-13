2abb560730ef107bd269f694f38e28426f5dbc60
Arezooshah <167054584+Arezooshah@users.noreply.github.com>
2025-12-13 23:08:58 +0330

    scrooltop

--- vite-project/src/screens/Landing/Landing.jsx
+++ vite-project/src/screens/Landing/Landing.jsx
@@ -7,6 +7,7 @@ import Slider2 from '../../Components/LandingComponent/ProfessorSlider/Slider2'
 import Services from '../../Components/LandingComponent/Services/Services'
 import News from '../../Components/LandingComponent/News/News'
 import SearchButton from '../../Components/common/SearchButton'
+import ScrollToTop from 'react-scroll-to-top'
 
 
 const Landing = () => {
@@ -18,6 +19,15 @@ const Landing = () => {
     <Slider2/>
     <Services/>
     <News/>
+    <ScrollToTop className="right-4 bottom-4 absolute"
+        smooth
+        style={{
+          backgroundColor: "rgba(88,0,255,0.4)",
+          right: "60px",
+          borderRadius: "50%",
+          textAlign: "center",
+        }}
+      />
     </div>
   )
 }
