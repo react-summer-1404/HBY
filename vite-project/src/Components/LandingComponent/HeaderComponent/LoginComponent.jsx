300270e174969d637adfb4c14482db34e90b7f8e
Arezooshah <167054584+Arezooshah@users.noreply.github.com>
2025-12-14 10:03:08 +0330

    font

--- vite-project/src/Components/LandingComponent/HeaderComponent/LoginComponent.jsx
+++ vite-project/src/Components/LandingComponent/HeaderComponent/LoginComponent.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom"; 
+import {getData} from '../../../core/localStorage/localStorage'
 
 const LoginComponent = () => {
   const navigate = useNavigate();
@@ -10,7 +11,8 @@ const LoginComponent = () => {
 
   return (
     <div className="flex justify-center items-center w-[150px] h-[40px] pr-[23px] pt-[6px] pl-[23px] gap-[8px] bg-[#9B0EE1] rounded-md text-amber-50">
-      <button onClick={handleLoginClick}>ورود/ثبت نام</button>
+      {!getData("token") && (<button onClick={handleLoginClick}>ورود/ثبت نام</button>)}
+      {getData("token") && (<button onClick={handleLoginClick}>پروفایل کاربر </button>)}
     </div>
   );
 };
